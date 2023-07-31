6c38d7778976d9b987dd48f2e1f27afec926e9d6
Matt Nappo <mattnappo@gmail.com>
2023-07-31 12:33:06 -0400

    Writing solver

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
 use itertools::iproduct;
+use rand::seq::SliceRandom;
+use rand::thread_rng;
 use std::convert::From;
 
 macro_rules! generate_from_impl {
@@ -74,19 +76,86 @@ impl From<(i32, i32, i32, i32)> for Card {
     }
 }
 
-#[derive(Debug)]
+/// An index in the hand
+struct Index(usize, usize, usize);
+
+/// A set
+struct Set(Card, Card, Card);
+
+impl Set {}
+
+/// A set finder
+/// Any struct that implements this trait can be benchmarked
+trait Finder {
+    fn find(&mut self, game: &Game) -> Index;
+}
+
+/// Run a user-supplied finder on a random game
+struct Solver<F: Finder> {
+    game: Game,
+    finder: F,
+}
+
+impl<F: Finder> Solver<F> {
+    pub fn new(finder: F) -> Solver<F> {
+        Solver {
+            game: Game::new(),
+            finder,
+        }
+    }
+
+    /// Play a full game using the user's finder
+    /// Return all the found sets
+    pub fn run(&mut self) -> Vec<Set> {}
+}
+
+/// The core state of a game of Set
 struct Game {
+    /// The cards left in the deck/hand
     deck: Vec<Card>,
-    position: usize,
+    /// The number of cards in the current hand
+    hand: usize,
+    /// The found sets so far. sets+deck = full 81 cards
+    sets: Vec<Set>,
 }
 
 impl Game {
     pub fn new() -> Self {
         Self {
-            deck: iproduct!(0..3, 0..3, 0..3, 0..3).map(Card::from).collect(),
-            position: 12,
+            deck: {
+                let mut d = iproduct!(0..3, 0..3, 0..3, 0..3)
+                    .map(Card::from)
+                    .collect::<Vec<Card>>();
+                d.shuffle(&mut thread_rng());
+                d
+            },
+            hand: 12,
+            sets: Vec::new(),
         }
     }
+
+    // See the current hand
+    pub fn hand(&self) -> &[Card] {
+        &self.deck[..self.hand]
+    }
+
+    fn get_card() {}
+
+    /* -- Gameplay functions -- */
+
+    /// Add a set to the found_sets iff it is valid, and draw 3 cards
+    /// if possible. Returns true iff set is valid
+    pub fn add_set(&mut self, cards: Index) -> bool {
+        // lookup cards given indices
+        // construct set object
+        // set.validate()
+        // remove cards from hand
+        // add 3 more cards from deck
+    }
+
+    /// Draw three more cards from the hand
+    /// Returns false if there are sets on the board
+    pub fn draw_three() {}
 }
 
 #[cfg(test)]
@@ -94,7 +163,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_card() {
+    fn test_game() {
         let game = Game::new();
         assert_eq!(game.deck.len(), 81);
     }
